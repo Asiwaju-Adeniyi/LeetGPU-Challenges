2eb29f75b26c3d6cecc40163025285378ccb479b
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2025-11-06 22:29:31 +0100

    Delete SimpleInference.py

--- SimpleInference.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-# input, model, and output are on the GPU
-def solve(input: torch.Tensor, model: nn.Module, output: torch.Tensor):
-    with torch.no_grad():
-        model_output = model(input)
-        output.copy_(model_output)
-    pass
